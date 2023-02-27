a31f74521a26b09d03ef8bcbcbc57d655e344727
Nick Sacco <nicksacco10@gmail.com>
2023-02-27 09:02:40 -0800

    Test: rider_log tests

--- spec/ride_spec.rb
+++ spec/ride_spec.rb
@@ -10,6 +10,9 @@ RSpec.describe Ride do
       admission_fee: 1,
       excitement: :gentle
       })
+      
+    @visitor1 = Visitor.new('Bruce', 54, '$10')
+    @visitor2 = Visitor.new('Tucker', 36, '$5')
   end
 
   it 'exists' do
@@ -22,5 +25,16 @@ RSpec.describe Ride do
     expect(@ride1.admission_fee).to eq(1)
     expect(@ride1.excitement).to eq(:gentle)
     expect(@ride1.total_revenue).to eq(0)
+  end
+
+  it 'can board riders and record how many times they rode' do
+    @visitor1.add_preference(:gentle)
+    @visitor2.add_preference(:gentle)
+
+    @ride1.board_rider(@visitor1)
+    @ride1.board_rider(@visitor2)
+    @ride1.board_rider(@visitor1)
+
+    expect(@ride1.rider_log).to eq(@visitor1 => 2, @visitor2 => 1)
   end
 end
