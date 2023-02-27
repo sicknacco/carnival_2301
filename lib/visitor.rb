bf9242f70fd6330a1efca0a07182f4198ba9bdc4
Nick Sacco <nicksacco10@gmail.com>
2023-02-27 08:16:05 -0800

    Feat: Init with attributes

--- lib/visitor.rb
+++ lib/visitor.rb
@@ -1,5 +1,13 @@
 class Visitor
+  attr_reader :name,
+              :height,
+              :spending_money,
+              :preferences
+
   def initialize(name, height, spending_money, preferences = [])
-    
+    @name = name
+    @height = height
+    @spending_money = spending_money.delete(spending_money[0]).to_i
+    @preferences = []
   end
 end
