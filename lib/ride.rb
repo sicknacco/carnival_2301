0f19b246fc054033aa08813e6655ac8a6f297b41
Nick Sacco <nicksacco10@gmail.com>
2023-02-27 08:52:29 -0800

    Feat: initialize w/ attributes

--- lib/ride.rb
+++ lib/ride.rb
@@ -1,4 +1,15 @@
 class Ride
-  def initialize(ride)
+  attr_reader :name,
+              :min_height,
+              :admission_fee,
+              :excitement,
+              :total_revenue
+
+  def initialize(hash)
+    @name = hash[:name]
+    @min_height = hash[:min_height]
+    @admission_fee = hash[:admission_fee]
+    @excitement = hash[:excitement]
+    @total_revenue = 0
   end
 end
