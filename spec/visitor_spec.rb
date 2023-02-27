bd51f99c3f8262cf0d7de43d38706f69d6e8f8e7
Nick Sacco <nicksacco10@gmail.com>
2023-02-27 08:08:26 -0800

    Test: init test, xit attributes tests

---
+++ spec/visitor_spec.rb
@@ -0,0 +1,19 @@
+require 'rspec'
+require './lib/visitor'
+
+RSpec.describe Visitor do
+  before(:each) do
+    @visitor1 = Visitor.new('Bruce', 54, '$10')
+  end
+
+  it 'exists' do
+    expect(@visitor1).to be_a Visitor
+  end
+
+  xit 'has attributes' do
+    expect(@visitor1.name).to eq("Bruce")
+    expect(@visitor1.height).to eq(54)
+    expect(@visitor1.spending_money).to eq(10)
+    expect(@visitor1.preferences).to eq([])
+  end
+end
