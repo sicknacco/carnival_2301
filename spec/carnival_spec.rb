5da8517c2bcc281e9f98f9f82a393ae2ba2aa3cf
Nick Sacco <nicksacco10@gmail.com>
2023-03-01 14:25:00 -0800

    Test: init tests

---
+++ spec/carnival_spec.rb
@@ -0,0 +1,21 @@
+require 'rspec'
+require './lib/visitor'
+require './lib/ride'
+require './lib/carnival'
+
+RSpec.describe Carnival do
+  before(:each) do
+    @carnival = Carnival.new("The Big Carnival", "3 weeks")
+  end
+
+  it 'exists' do
+    expect(@carnival).to be_a(Carnival)
+  end
+
+end
+#### 1) Each carnival has a duration, as well as a way to read that data
+#### 2) Each carnival has rides and can list those rides.
+#### 3) At LEAST ONE of the following:
+### A carnival can tell us its most popular ride.
+### A carnival can tell us its most profitable ride
+### A carnival can calculate the total revenue earned from all its rides.
