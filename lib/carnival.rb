289a11a0501b2610a1ae6a7a9c095c8c44aed194
Nick Sacco <nicksacco10@gmail.com>
2023-03-01 15:01:25 -0800

    Feat: add ride method

--- lib/carnival.rb
+++ lib/carnival.rb
@@ -1,10 +1,15 @@
 class Carnival
-attr_reader :name,
+  attr_reader :name,
             :duration,
             :rides
+
   def initialize(name, duration)
     @name = name
     @duration = duration
     @rides = []
+  end
+
+  def add_ride(ride)
+    @rides << ride
   end
 end
