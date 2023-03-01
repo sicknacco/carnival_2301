4342415f63d27eef9d34245236c5e30a464b5f11
Nick Sacco <nicksacco10@gmail.com>
2023-03-01 14:59:53 -0800

    Test: add ride tests

--- spec/carnival_spec.rb
+++ spec/carnival_spec.rb
@@ -6,6 +6,9 @@ require './lib/carnival'
 RSpec.describe Carnival do
   before(:each) do
     @carnival = Carnival.new("The Big Carnival", "3 weeks")
+    @ride1 = Ride.new({name: 'Carousel', min_height: 24, admission_fee: 1, excitement: :gentle})
+    @ride2 = Ride.new({ name: 'Ferris Wheel', min_height: 36, admission_fee: 5, excitement: :gentle })
+    @ride3 = Ride.new({ name: 'Roller Coaster', min_height: 54, admission_fee: 2, excitement: :thrilling })
   end
 
   it 'exists and has a name and duration' do
@@ -18,6 +21,16 @@ RSpec.describe Carnival do
     expect(@carnival.rides).to eq([])
   end
 
+  it 'can add rides to rides array' do
+    @carnival.add_ride(@ride1)
+
+    expect(@carnival.rides).to eq([@ride1])
+
+    @carnival.add_ride(@ride2)
+    @carnival.add_ride(@ride3)
+    
+    expect(@carnival.rides).to eq([@ride1, @ride2, @ride3])
+  end
 end
 #### 1) Each carnival has a duration, as well as a way to read that data
 #### 2) Each carnival has rides and can list those rides.
